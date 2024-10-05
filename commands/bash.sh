bd47ea58da2e22c1705edb86e7c8ca6c6169eb0b
Yinda Zhang <1293516092@qq.com>
2024-10-05 13:27:45 +0800

    Update bash.sh

--- commands/bash.sh
+++ commands/bash.sh
@@ -1,4 +1,24 @@
-sudo killall -9 ns3.42-header-compress
+sudo chmod -R 777 /mydata/
 
-nohup ./ns3 run "scratch/header-compress --ip_version=0" > 0.txt &
-nohup ./ns3 run "scratch/header-compress --ip_version=1" > 1.txt &
+cd /mydata
+git clone https://github.com/yindazhang/header-compress.git
+
+wget https://www.nsnam.org/releases/ns-allinone-3.37.tar.bz2
+tar xjf ns-allinone-3.37.tar.bz2
+
+rm -rf ns-allinone-3.37/ns-3.37/src/ ns-allinone-3.37/ns-3.37/scratch/
+cp -r header-compress/src/ ns-allinone-3.37/ns-3.37/
+cp -r header-compress/scratch/ ns-allinone-3.37/ns-3.37/
+cp -r header-compress/commands/ ns-allinone-3.37/ns-3.37/
+
+cd ns-allinone-3.37/ns-3.37/
+./ns3 configure --build-profile=optimized --out=build/optimized
+./ns3 build
+
+sudo killall -9 ns3.37-header-compress
+
+# cd commands/
+# python3 generate_cmd.py
+# python3 traffic_gen.py -l 0.8
+
+# nohup ./ns3 run "scratch/header-compress --ip_version=0" > 0.txt &
