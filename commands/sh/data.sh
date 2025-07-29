e0d5553de44d0432171291d1999abbf39e5bd43f
Yinda Zhang <1293516092@qq.com>
2025-07-28 17:14:23 -0700

    Update data.sh

--- commands/sh/data.sh
+++ commands/sh/data.sh
@@ -14,6 +14,14 @@ cd "$NS_DIR/ns-$VERSION/commands/"
 CDFS=("VL2" "WebSearch" "Cache" "Hadoop" "RPC")
 LOADS=(0.4 0.5 0.6 0.7 0.8)
 declare -A DURATION=( ["VL2"]=1.0 ["WebSearch"]=0.5 ["Cache"]=0.5 ["Hadoop"]=0.5 ["RPC"]=0.2 )
+
+# Run ML_gen.py for each load
+for load in "${LOADS[@]}"; do
+    python3 ML_gen.py -l "$load" -t 1.0 &
+done
+wait
+echo "Finished ML"
+
 # Run traffic_gen.py for each CDF and load with corresponding duration
 for cdf in "${CDFS[@]}"; do
     duration=${DURATION[$cdf]}
