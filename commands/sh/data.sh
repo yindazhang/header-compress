0c6f16406cf44111fa591c1f385dd02d86c6fba3
yindaz <yindaz@node0.flowzip.pennnetworks-pg0.wisc.cloudlab.us>
2025-08-17 02:04:24 -0500

    Storage

--- commands/sh/data.sh
+++ commands/sh/data.sh
@@ -11,13 +11,13 @@ cp -r "$HEADER_DIR/commands/" "$NS_DIR/ns-$VERSION/"
 # Change to ns directory
 cd "$NS_DIR/ns-$VERSION/commands/"
 # Define CDFs, loads, and durations for each CDF
-CDFS=("VL2" "WebSearch" "Cache" "Hadoop" "RPC")
+CDFS=("Storage" "WebSearch" "Cache" "Hadoop" "RPC")
 LOADS=(0.4 0.5 0.6 0.7 0.8)
-declare -A DURATION=( ["VL2"]=1.0 ["WebSearch"]=0.5 ["Cache"]=0.5 ["Hadoop"]=0.5 ["RPC"]=0.2 )
+declare -A DURATION=( ["Storage"]=0.5 ["WebSearch"]=0.5 ["Cache"]=0.5 ["Hadoop"]=0.5 ["RPC"]=0.2 )
 
 # Run ML_gen.py for each load
 for load in "${LOADS[@]}"; do
-    python3 ML_gen.py -l "$load" -t 1.0 &
+    python3 ML_gen.py -l "$load" -t 0.5 &
 done
 wait
 echo "Finished ML"
